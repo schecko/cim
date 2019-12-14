36c4ee6403cba8c74c0f277ee4190da496c82def
Scott Checko <schecko@sfu.ca>
2019-12-13 21:56:52 -0800

    rewriting rendering, using instancing, breaking things

--- src/main.rs
+++ src/main.rs
@@ -70,12 +70,14 @@ static RECT: [[f32; 3]; 6] = [
 static VERTEX: &str = r#"
     #version 330 core
     layout (location = 0) in vec3 aPos;
+    layout (location = 1) in vec3 aColor;
+    layout (location = 2) in vec3 aOffset;
     uniform mat4 model;
     uniform mat4 view;
     uniform mat4 proj;
 
     void main() {
-        gl_Position = proj * view * model * vec4(aPos, 1.0);
+        gl_Position = proj * view * model * vec4(aPos + aOffset, 1.0);
     }
 "#;
 
@@ -87,49 +89,6 @@ static FRAGMENT: &str = r#"
         FragColor = vec4(1.0, 1.0, 1.0, 1.0);
     }
 "#;
-
-struct Renderer {
-    solid: Pipeline,
-    vao: u32,
-    vbo: u32,
-}
-
-fn render(gamestate: &GameState, renderer: &Renderer) {
-
-    unsafe {
-        gl::ClearColor(1.0, 0.5, 0.7, 1.0);
-        gl::Clear(gl::COLOR_BUFFER_BIT);
-
-        renderer.solid.set_use();
-
-        let decomp = Decomposed {
-            scale: 1.0,
-            rot: Quaternion::new(0.0f32, 0.0, 0.0, 0.0),
-            disp: Vector3::new(0.0, 0.0, 0.0),
-        };
-
-        let proj: Matrix4<f32> = perspective(Deg(45.0), 1.0, 0.1, 1000.0);
-        let view: Matrix4<f32> = Decomposed {
-            scale: 1.0,
-            rot: Quaternion::new(0.0f32, 0.0, 0.0, 0.0),
-            disp: Vector3::new(0.0f32, 0.0, -100.0),
-        }.into();
-
-        let model: Matrix4<f32> = decomp.into();
-
-        let model_loc = renderer.solid.get_uniform_location("model");
-        let view_loc = renderer.solid.get_uniform_location("view");
-        let proj_loc = renderer.solid.get_uniform_location("proj");
-
-        gl::UniformMatrix4fv(model_loc, 1, gl::FALSE, model.as_ptr());
-        gl::UniformMatrix4fv(view_loc, 1, gl::FALSE, view.as_ptr());
-        gl::UniformMatrix4fv(proj_loc, 1, gl::FALSE, proj.as_ptr());
-
-        gl::BindVertexArray(renderer.vao);
-
-        gl::DrawArrays(gl::TRIANGLES, 0, (gamestate.grid.len() * 6) as i32);
-    }
-}
 
 fn main() -> Result<(), String> {
     let event_loop = EventLoop::new();
@@ -144,31 +103,37 @@ fn main() -> Result<(), String> {
     load(context.context());
     let solid = Pipeline::new(VERTEX, FRAGMENT)?;
 
-    let gamestate = GameState::new();
+    let game_state = GameState::new();
 
-    let (vao, vbo) = unsafe {
+    let (vao, quad_vbo, instance_vbo) = unsafe {
         let mut vao: u32 = 0;
         gl::GenVertexArrays(1, &mut vao as *mut _);
-        let mut vbo: u32 = 0;
-        gl::GenBuffers(1, &mut vbo as *mut _);
+        let mut quad_vbo: u32 = 0;
+        gl::GenBuffers(1, &mut quad_vbo as *mut _);
+        let mut instance_vbo: u32 = 0;
+        gl::GenBuffers(1, &mut instance_vbo as *mut _);
 
-        gl::EnableVertexAttribArray(vao);
+        gl::BindBuffer(gl::ARRAY_BUFFER, quad_vbo);
+        gl::BufferData(gl::ARRAY_BUFFER, mem::size_of_val(&RECT) as isize, RECT.as_ptr() as *mut _, gl::STATIC_DRAW);
+        assert!(gl::GetError() == 0);
+
         gl::BindVertexArray(vao);
-        gl::BindBuffer(gl::ARRAY_BUFFER, vbo);
-        gl::BufferData(gl::ARRAY_BUFFER, (draw_buffer.len() * mem::size_of_val(&RECT)) as isize, draw_buffer.as_ptr() as *mut _, gl::STATIC_DRAW);
-
-
-        gl::VertexAttribPointer(0, 3, gl::FLOAT, gl::FALSE, 3 * mem::size_of::<f32>() as i32, std::ptr::null());
 
         gl::EnableVertexAttribArray(0);
-        gl::BindBuffer(gl::ARRAY_BUFFER, 0);
-        (vao, vbo)
-    };
+        gl::VertexAttribPointer(0, 3, gl::FLOAT, gl::FALSE, 6 * mem::size_of::<f32>() as i32, std::ptr::null());
+        gl::EnableVertexAttribArray(1);
+        gl::VertexAttribPointer(1, 3, gl::FLOAT, gl::FALSE, 6 * mem::size_of::<f32>() as i32, (3 * mem::size_of::<f32>()) as *const _);
 
-    let renderer = Renderer {
-        solid,
-        vao,
-        vbo,
+        gl::EnableVertexAttribArray(2);
+        gl::BindBuffer(gl::ARRAY_BUFFER, instance_vbo);
+        gl::VertexAttribPointer(2, 3, gl::FLOAT, gl::FALSE, 3 * mem::size_of::<f32>() as i32, std::ptr::null());
+        gl::VertexAttribDivisor(2, 1);
+        assert!(gl::GetError() == 0);
+
+        // reset opengl state
+        gl::BindVertexArray(0);
+        gl::BindBuffer(gl::ARRAY_BUFFER, 0);
+        (vao, quad_vbo, instance_vbo)
     };
 
     event_loop.run(move |event, _, control_flow| {
@@ -189,7 +154,48 @@ fn main() -> Result<(), String> {
             _ => { },
         };
 
-        render(&gamestate, &renderer);
+        let mut rect_positions: Vec<_> = game_state.grid.indexed_iter().map(|((x, y), grid)| {
+            Vector3::new(x as f32, y as f32, 0.0)
+        }).collect();
+
+        unsafe {
+            gl::BindBuffer(gl::ARRAY_BUFFER, quad_vbo);
+            gl::BufferData(gl::ARRAY_BUFFER, (rect_positions.len() * mem::size_of_val(&rect_positions[0])) as isize, rect_positions.as_mut_ptr() as *mut _, gl::STATIC_DRAW);
+            assert!(gl::GetError() == 0);
+
+            gl::ClearColor(1.0, 0.5, 0.7, 1.0);
+            gl::Clear(gl::COLOR_BUFFER_BIT);
+
+            solid.set_use();
+
+            let decomp = Decomposed {
+                scale: 1.0,
+                rot: Quaternion::new(0.0f32, 0.0, 0.0, 0.0),
+                disp: Vector3::new(0.0, 0.0, 0.0),
+            };
+
+            let proj: Matrix4<f32> = perspective(Deg(45.0), 1.0, 0.1, 1000.0);
+            let view: Matrix4<f32> = Decomposed {
+                scale: 1.0,
+                rot: Quaternion::new(0.0f32, 0.0, 0.0, 0.0),
+                disp: Vector3::new(0.0f32, 0.0, -100.0),
+            }.into();
+
+            let model: Matrix4<f32> = decomp.into();
+
+            let model_loc = solid.get_uniform_location("model");
+            let view_loc = solid.get_uniform_location("view");
+            let proj_loc = solid.get_uniform_location("proj");
+
+            gl::UniformMatrix4fv(model_loc, 1, gl::FALSE, model.as_ptr());
+            gl::UniformMatrix4fv(view_loc, 1, gl::FALSE, view.as_ptr());
+            gl::UniformMatrix4fv(proj_loc, 1, gl::FALSE, proj.as_ptr());
+
+            gl::BindVertexArray(vao);
+            assert!(gl::GetError() == 0);
+            gl::DrawArraysInstanced(gl::TRIANGLES, 0, RECT.len() as i32, rect_positions.len() as i32);
+            assert!(gl::GetError() == 0);
+        }
         context.swap_buffers().unwrap();
     });
 
