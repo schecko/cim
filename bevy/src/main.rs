624f3f06cdada2ca989802f1bed196b23514f626
Scott Checko <schecko@sfu.ca>
2021-12-11 23:17:48 -0800

    starting to refactor

--- bevy/src/main.rs
+++ bevy/src/main.rs
@@ -14,6 +14,7 @@ enum GameState {
 }
 
 struct ScoreBoardText;
+struct Controllable;
 
 fn main() {
     App::build()
@@ -45,15 +46,9 @@ fn main() {
         )
         .add_system_set(
             SystemSet::on_update(GameState::Playing)
-                .with_system(move_player.system())
+                .with_system(move_active_unit.system())
                 .with_system(focus_camera.system())
-                .with_system(rotate_bonus.system())
                 .with_system(scoreboard_system.system())
-        )
-        .add_system_set(
-            SystemSet::on_update(GameState::Playing)
-                .with_run_criteria(FixedTimestep::step(5.0))
-                .with_system(spawn_bonus.system())
         )
         .add_system_set(
             SystemSet::on_exit(GameState::Playing)
@@ -82,25 +77,20 @@ struct Cell {
 }
 
 #[derive(Default)]
-struct Player {
-    entity: Option<Entity>,
+struct BoardUnit {
     i: usize,
     j: usize,
 }
 
 #[derive(Default)]
-struct Bonus {
-    entity: Option<Entity>,
-    i: usize,
-    j: usize,
+struct Settlement {
     handle: Handle<Scene>,
 }
 
 #[derive(Default)]
 struct Game {
     board: Vec<Vec<Cell>>,
-    player: Player,
-    bonus: Bonus,
+    active_unit: Option<Entity>,
     score: i32,
     cake_eaten: u32,
     camera_should_focus: Vec3,
@@ -135,8 +125,6 @@ fn setup(mut commands: Commands, asset_server: Res<AssetServer>, mut game: ResMu
     // reset the game state
     game.cake_eaten = 0;
     game.score = 0;
-    game.player.i = BOARD_SIZE_I / 2;
-    game.player.j = BOARD_SIZE_J / 2;
 
     commands.spawn_bundle(LightBundle {
         transform: Transform::from_xyz(4.0, 5.0, 4.0),
@@ -152,7 +140,7 @@ fn setup(mut commands: Commands, asset_server: Res<AssetServer>, mut game: ResMu
                     let height = rand::thread_rng().gen_range(-0.1..0.1);
                     commands
                         .spawn_bundle((
-                            Transform::from_xyz(i as f32, height - 0.2, j as f32),
+                            Transform::from_xyz(i as f32, height - 0.2, j as f32) * Transform::from_scale(Vec3::new(1.0, 2.0, 1.0)),
                             GlobalTransform::identity(),
                         ))
                         .with_children(|cell| {
@@ -164,15 +152,16 @@ fn setup(mut commands: Commands, asset_server: Res<AssetServer>, mut game: ResMu
         })
         .collect();
 
-    // spawn the game character
-    game.player.entity = Some(
+    let settler_unit = BoardUnit{ i: BOARD_SIZE_I / 2, j: BOARD_SIZE_J / 2 };
+    // spawn the initial settler
+    game.active_unit = Some(
         commands
             .spawn_bundle((
                 Transform {
                     translation: Vec3::new(
-                        game.player.i as f32,
-                        game.board[game.player.j][game.player.i].height,
-                        game.player.j as f32,
+                        settler_unit.i as f32,
+                        game.board[settler_unit.j][settler_unit.i].height,
+                        settler_unit.j as f32,
                     ),
                     rotation: Quat::from_rotation_y(-std::f32::consts::FRAC_PI_2),
                     ..Default::default()
@@ -182,11 +171,13 @@ fn setup(mut commands: Commands, asset_server: Res<AssetServer>, mut game: ResMu
             .with_children(|cell| {
                 cell.spawn_scene(asset_server.load("models/AlienCake/alien.glb#Scene0"));
             })
-            .id(),
+            .insert(settler_unit)
+            .insert(Controllable{})
+            .id()
     );
 
     // load the scene for the cake
-    game.bonus.handle = asset_server.load("models/AlienCake/cakeBirthday.glb#Scene0");
+    //game.bonus.handle = asset_server.load("models/AlienCake/cakeBirthday.glb#Scene0");
 
     // scoreboard
     commands.spawn_bundle(TextBundle {
@@ -221,38 +212,51 @@ fn teardown(mut commands: Commands, entities: Query<Entity, Without<Camera>>) {
 }
 
 // control the game character
-fn move_player(
+fn move_active_unit(
     mut commands: Commands,
     keyboard_input: Res<Input<KeyCode>>,
     mut game: ResMut<Game>,
-    mut transforms: Query<&mut Transform>,
+    mut board_unit_transforms: Query<(&mut Transform, &mut BoardUnit), With<Controllable>>,
 ) {
+    let active_unit = if let Some(id) = game.active_unit {
+        id
+    } else {
+        return;
+    };
+
+    let (mut transform, mut board_unit) = if let Ok((transform, board_unit)) = board_unit_transforms.get_mut(active_unit) {
+        (transform, board_unit)
+    } else {
+        game.active_unit = None;
+        return;
+    };
+
     let mut moved = false;
     let mut rotation = 0.0;
     if keyboard_input.just_pressed(KeyCode::Up) {
-        if game.player.i < BOARD_SIZE_I - 1 {
-            game.player.i += 1;
+        if board_unit.i < BOARD_SIZE_I - 1 {
+            board_unit.i += 1;
         }
         rotation = -std::f32::consts::FRAC_PI_2;
         moved = true;
     }
     if keyboard_input.just_pressed(KeyCode::Down) {
-        if game.player.i > 0 {
-            game.player.i -= 1;
+        if board_unit.i > 0 {
+            board_unit.i -= 1;
         }
         rotation = std::f32::consts::FRAC_PI_2;
         moved = true;
     }
     if keyboard_input.just_pressed(KeyCode::Right) {
-        if game.player.j < BOARD_SIZE_J - 1 {
-            game.player.j += 1;
+        if board_unit.j < BOARD_SIZE_J - 1 {
+            board_unit.j += 1;
         }
         rotation = std::f32::consts::PI;
         moved = true;
     }
     if keyboard_input.just_pressed(KeyCode::Left) {
-        if game.player.j > 0 {
-            game.player.j -= 1;
+        if board_unit.j > 0 {
+            board_unit.j -= 1;
         }
         rotation = 0.0;
         moved = true;
@@ -260,25 +264,21 @@ fn move_player(
 
     // move on the board
     if moved {
-        *transforms.get_mut(game.player.entity.unwrap()).unwrap() = Transform {
+        transform.translation = Vec3::new(
+            board_unit.i as f32,
+            game.board[board_unit.j][board_unit.i].height,
+            board_unit.j as f32,
+        );
+        transform.rotation = Quat::from_rotation_y(rotation);
+        /*transform = Transform {
             translation: Vec3::new(
-                game.player.i as f32,
-                game.board[game.player.j][game.player.i].height,
-                game.player.j as f32,
+                board_unit.i as f32,
+                game.board[board_unit.j][board_unit.i].height,
+                board_unit.j as f32,
             ),
             rotation: Quat::from_rotation_y(rotation),
             ..Default::default()
-        };
-    }
-
-    // eat the cake!
-    if let Some(entity) = game.bonus.entity {
-        if game.player.i == game.bonus.i && game.player.j == game.bonus.j {
-            game.score += 2;
-            game.cake_eaten += 1;
-            commands.entity(entity).despawn_recursive();
-            game.bonus.entity = None;
-        }
+        };*/
     }
 }
 
@@ -290,7 +290,7 @@ fn focus_camera(
 ) {
     const SPEED: f32 = 2.0;
     // if there is both a player and a bonus, target the mid-point of them
-    if let (Some(player_entity), Some(bonus_entity)) = (game.player.entity, game.bonus.entity) {
+    /*if let (Some(player_entity), Some(bonus_entity)) = (game.player.entity, game.bonus.entity) {
         if let (Ok(player_transform), Ok(bonus_transform)) = (
             transforms.q1().get(player_entity),
             transforms.q1().get(bonus_entity),
@@ -322,66 +322,7 @@ fn focus_camera(
         if camera.name == Some(CAMERA_3D.to_string()) {
             *transform = transform.looking_at(game.camera_is_focus, Vec3::Y);
         }
-    }
-}
-
-// despawn the bonus if there is one, then spawn a new one at a random location
-fn spawn_bonus(
-    mut state: ResMut<State<GameState>>,
-    mut commands: Commands,
-    mut game: ResMut<Game>,
-) {
-    if *state.current() != GameState::Playing {
-        return;
-    }
-    if let Some(entity) = game.bonus.entity {
-        game.score -= 3;
-        commands.entity(entity).despawn_recursive();
-        game.bonus.entity = None;
-        if game.score <= -5 {
-            state.set(GameState::GameOver).unwrap();
-            return;
-        }
-    }
-
-    // ensure bonus doesn't spawn on the player
-    loop {
-        game.bonus.i = rand::thread_rng().gen_range(0..BOARD_SIZE_I);
-        game.bonus.j = rand::thread_rng().gen_range(0..BOARD_SIZE_J);
-        if game.bonus.i != game.player.i || game.bonus.j != game.player.j {
-            break;
-        }
-    }
-    game.bonus.entity = Some(
-        commands
-            .spawn_bundle((
-                Transform {
-                    translation: Vec3::new(
-                        game.bonus.i as f32,
-                        game.board[game.player.j][game.player.i].height + 0.2,
-                        game.bonus.j as f32,
-                    ),
-                    ..Default::default()
-                },
-                GlobalTransform::identity(),
-            ))
-            .with_children(|cell| {
-                cell.spawn_scene(game.bonus.handle.clone());
-            })
-            .id(),
-    );
-}
-
-// let the cake turn on itself
-fn rotate_bonus(game: Res<Game>, time: Res<Time>, mut transforms: Query<&mut Transform>) {
-    if let Some(entity) = game.bonus.entity {
-        if let Ok(mut cake_transform) = transforms.get_mut(entity) {
-            cake_transform.rotate(Quat::from_rotation_y(time.delta_seconds()));
-            cake_transform.scale = Vec3::splat(
-                1.0 + (game.score as f32 / 10.0 * time.seconds_since_startup().sin() as f32).abs(),
-            );
-        }
-    }
+    }*/
 }
 
 // update the score displayed during the game
@@ -396,7 +337,6 @@ fn gameover_keyboard(mut state: ResMut<State<GameState>>, keyboard_input: Res<In
     }
 }
 
-// restart the game when pressing spacebar
 fn enter_game(mut state: ResMut<State<GameState>>, keyboard_input: Res<Input<KeyCode>>) {
     if keyboard_input.just_pressed(KeyCode::Space) {
         state.set(GameState::Playing).unwrap();
