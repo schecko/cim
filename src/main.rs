33d5ca0c5a445dae35364896bd0290d9312cada3
Scott Checko <schecko@sfu.ca>
2024-11-24 21:30:53 -0800

    hello world bevy

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,24 @@
-fn main() {
-    println!("Hello, world!");
+use bevy::prelude::*;
+
+fn hello_world() {
+    println!("hello world!");
+}
+
+pub struct HelloPlugin;
+
+impl Plugin for HelloPlugin
+{
+    fn build(&self, app: &mut App)
+    {
+        app.add_systems(Update, hello_world);
+        println!("hello world2!");
+    }
+}
+
+fn main()
+{
+    App::new()
+        .add_plugins(HelloPlugin)
+        .add_plugins(DefaultPlugins)
+        .run();
 }
