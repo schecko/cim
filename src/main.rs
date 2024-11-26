9e09cf3f44f96c91e7d1d2af21996bba10a5012d
Scott Checko <schecko@sfu.ca>
2024-11-25 22:12:23 -0800

    adding fps counter

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,8 @@
-use bevy::prelude::*;
+use bevy::
+{
+    dev_tools::fps_overlay::{FpsOverlayConfig, FpsOverlayPlugin},
+    prelude::*,
+};
 
 fn hello_world() {
     println!("hello world!");
@@ -15,10 +19,34 @@ impl Plugin for HelloPlugin
     }
 }
 
+fn setup_camera(mut commands: Commands) {
+    commands.spawn
+    (
+        Camera2dBundle
+        {
+            transform: Transform::from_xyz(100.0, 200.0, 0.0),
+            ..default()
+        }
+    );
+}
+
 fn main()
 {
     App::new()
         .add_plugins(HelloPlugin)
         .add_plugins(DefaultPlugins)
+        .add_plugins(FpsOverlayPlugin
+        {
+            config: FpsOverlayConfig
+            {
+                text_config: TextStyle
+                {
+                    font_size: 50.0,
+                    color: Color::srgb(0.0, 1.0, 0.0),
+                    font: default(),
+                },
+            },
+        })
+        .add_systems(Startup, setup_camera)
         .run();
 }
