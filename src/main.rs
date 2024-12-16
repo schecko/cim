313e18e23500e04241b8becf3a07cd3c3f31f247
Scott Checko <schecko@sfu.ca>
2024-12-15 20:35:00 -0800

    cleanup

--- src/main.rs
+++ src/main.rs
@@ -102,48 +102,41 @@ fn setup
     // can't load sync?
     let _board_vis_tuning = asset_server.load::<BoardVisTuning>("tuning/board_vis.ron");
 
-    let mesh = if false
-    {
-        meshes.add(Rectangle::default())
-    }
-    else
-    {
-        let mut v_pos = vec!
-        [
-            [0.0, 0.0, 0.0], // TL
-            [1.0, 0.0, 0.0], // TR
-            [0.0, 1.0, 0.0], // BL
-            [1.0, 1.0, 0.0], // BR
-        ];
-        let mut v_color: Vec<[f32; 4]> = vec![LinearRgba::WHITE.to_f32_array(); 4];
-        let mut v_uv: Vec<[f32; 2]> = vec!
-        [
-            [0.0, 1.0],
-            [1.0, 1.0],
-            [0.0, 0.0],
-            [1.0, 0.0],
-        ];
-        let mut v_normal: Vec<[f32; 3]> = vec![[0.0, 0.0, 1.0]; 4];
+    let v_pos = vec!
+    [
+        [0.0, 0.0, 0.0], // TL
+        [1.0, 0.0, 0.0], // TR
+        [0.0, 1.0, 0.0], // BL
+        [1.0, 1.0, 0.0], // BR
+    ];
+    let v_color: Vec<[f32; 4]> = vec![LinearRgba::WHITE.to_f32_array(); 4];
+    let v_uv: Vec<[f32; 2]> = vec!
+    [
+        [0.0, 1.0],
+        [1.0, 1.0],
+        [0.0, 0.0],
+        [1.0, 0.0],
+    ];
+    let v_normal: Vec<[f32; 3]> = vec![[0.0, 0.0, 1.0]; 4];
 
-        let mut indices = vec!
-        [
-            0, 2, 1,
-            1, 2, 3
-        ];
+    let indices = vec!
+    [
+        0, 2, 1,
+        1, 2, 3
+    ];
 
-        let mut mesh = Mesh::new
-        (
-            PrimitiveTopology::TriangleList,
-            RenderAssetUsages::RENDER_WORLD,
-        );
-        mesh.insert_attribute(Mesh::ATTRIBUTE_POSITION, v_pos);
-        mesh.insert_attribute(Mesh::ATTRIBUTE_COLOR, v_color);
-        mesh.insert_attribute(Mesh::ATTRIBUTE_UV_0, v_uv);
-        mesh.insert_attribute(Mesh::ATTRIBUTE_NORMAL, v_normal);
-        mesh.insert_indices(bevy::render::mesh::Indices::U32(indices));
+    let mut mesh = Mesh::new
+    (
+        PrimitiveTopology::TriangleList,
+        RenderAssetUsages::RENDER_WORLD,
+    );
+    mesh.insert_attribute(Mesh::ATTRIBUTE_POSITION, v_pos);
+    mesh.insert_attribute(Mesh::ATTRIBUTE_COLOR, v_color);
+    mesh.insert_attribute(Mesh::ATTRIBUTE_UV_0, v_uv);
+    mesh.insert_attribute(Mesh::ATTRIBUTE_NORMAL, v_normal);
+    mesh.insert_indices(bevy::render::mesh::Indices::U32(indices));
 
-        meshes.add(mesh)
-    };
+    let mesh_id = meshes.add(mesh);
 
     for pos in size.positions_row_major()
     {
@@ -152,7 +145,7 @@ fn setup
         commands
             .spawn
             ((
-                Mesh2d(mesh.clone().into()),
+                Mesh2d(mesh_id.clone().into()),
                 MeshMaterial2d(custom_material.clone().into()),
                 Transform::default()
                     .with_translation(translation)
