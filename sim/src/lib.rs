3326d9aa7770a310ce2f86e58e71b94443d07758
Scott Checko <schecko@sfu.ca>
2025-01-15 01:40:54 -0800

    working on mine art

--- sim/src/lib.rs
+++ sim/src/lib.rs
@@ -1,32 +1,5 @@
 
-use base::array2;
-
-#[repr(u8)]
-#[derive(Debug, Default, Clone, Copy, PartialEq, Eq, PartialOrd, Ord, Hash)]
-pub enum CellState
-{
-    #[default]
-    None = 0,
-    Mine = 1,
-    NonPlayable = 2,
-}
-
-#[derive(Debug, Clone)]
-struct Board
-{
-    states: array2::Array2<CellState>,
-}
-
-impl Board
-{
-    pub fn new(width: i32, height: i32) -> Self
-    {
-        Self
-        {
-            states: array2::Array2::new(width, height),
-        }
-    }
-}
+mod grid;
 
 pub fn hello_sim()
 {
